bd867b21e2fde0d52b9ec96249b2417828ed40b6
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2018-06-05 09:58:50 -0700

    Initial Commit with Java 10

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+/**
+ * The main driver of the program. This file will create the game, create the two agents,
+ * and create the window for the game. After that, Connect4Frame runs everything.
+ * 
+ * You should only modify this class to change which agents are playing.
+ * Your options are:
+ * <ul>
+ * <li>RandomAgent</li>
+ * <li>BeginnerAgent</li>
+ * <li>IntermediateAgent</li>
+ * <li>AdvancedAgent</li>
+ * <li>BrilliantAgent</li>
+ * </ul>
+ */
+public class Main {
+    /**
+     * Main method that initializes the game.
+     * @param args
+     *      command line arguments
+     */
+    public static void main(String[] args) {
+        // create the game; these sizes can be altered for larger or smaller games
+        Connect4Game game = new Connect4Game(7, 6);
+        // create the red player, any subclass of Agent
+        Agent redPlayer = new RandomAgent(game, true);
+        // create the yellow player, any subclass of Agent
+        Agent yellowPlayer = new BeginnerAgent(game, false);
+        // create the game window
+        Connect4Frame mainframe = new Connect4Frame(game, redPlayer, yellowPlayer);
+    }
+}
---
+++ src/test/java/MyAgentTest.java
@@ -0,0 +1,172 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyAgentTest {
+
+  Connect4Game game; 
+
+
+  @Before
+  public void setUp() throws Exception {
+    game = new Connect4Game(7, 6);
+  }
+  
+  @Test
+  public void testICanWinVerticallySimple() {
+    MyAgent redAgent = new MyAgent(game, true);
+    MyAgent yellowAgent = new MyAgent(game, false);
+    game.clearBoard();
+    for (int i = 0; i < 3; i++) {
+      redAgent.moveOnColumn(1);
+      yellowAgent.moveOnColumn(2);
+    }
+    
+    assertEquals(redAgent.iCanWin(), 1);
+    
+  }
+  
+  @Test
+  public void testICanWinVerticallyTop4() {
+    MyAgent redAgent = new MyAgent(game, true);
+    MyAgent yellowAgent = new MyAgent(game, false);
+    game.clearBoard();
+    for (int i = 0; i < 2; i++) {
+      redAgent.moveOnColumn(1);
+      yellowAgent.moveOnColumn(2);
+    }
+    
+    for (int i = 0; i < 3; i++) {
+      redAgent.moveOnColumn(2);
+      yellowAgent.moveOnColumn(1);   
+    }
+    
+    assertEquals(redAgent.iCanWin(), 2);
+    
+  }
+  
+  // TODO: Write 2 test cases for testICanWinHorizontally 
+  
+  // TODO: Write 2 test cases for testICanWinDiagonally
+
+  @Test
+  public void testTheyCanWin() {
+    MyAgent redAgent = new MyAgent(game, true);
+    MyAgent yellowAgent = new MyAgent(game, false);
+    game.clearBoard();
+    for (int i = 0; i < 3; i++) {
+      redAgent.moveOnColumn(1);
+      yellowAgent.moveOnColumn(2);
+    }
+    
+    assertEquals(redAgent.theyCanWin(), 2);
+  }
+  
+  // TODO: Write testTheyCanWinHorizontally
+  
+  // TODO: Write testTheyCanWinDiagonally
+
+  // Tests you can win against a Beginner agent as Red
+  @Test (timeout=1000)
+  public void testRedWinningBeginnerAgent() {
+    Agent redAgent = new MyAgent(game, true);
+    Agent yellowAgent = new BeginnerAgent(game, false);
+    int numberOfWins = 0;
+    for (int i = 0; i < 50; i++) {
+      game.clearBoard(); 
+      while(!game.boardFull() || game.gameWon() == 'N') {
+        redAgent.move();
+        if (game.gameWon() != 'R') {
+          yellowAgent.move();
+        }
+      }
+
+      if (game.gameWon() == 'R') {
+        numberOfWins++;
+      }
+    }
+    System.out.println("You won: " + numberOfWins + " games as Red against Beginner");
+    // Test that you win over 90% of your games
+    assertTrue(numberOfWins >= 45);
+  }
+
+  // Tests you can win against a Beginner agent as Yellow
+  @Test (timeout=1000)
+  public void testYellowWinningBeginnerAgent() {
+    Agent redAgent = new BeginnerAgent(game, true);
+    Agent yellowAgent = new MyAgent(game, false);
+    int numberOfWins = 0;
+    for (int i = 0; i < 50; i++) {
+      game.clearBoard(); 
+      while(!game.boardFull() || game.gameWon() == 'N') {
+        redAgent.move();
+        if (game.gameWon() != 'R') {
+          yellowAgent.move();
+        }
+      }
+
+      if (game.gameWon() == 'Y') {
+        numberOfWins++;
+      }
+    }
+    System.out.println("You won: " + numberOfWins + " games as Yellow against Beginner");
+    // Test that you win over 90% of your games
+    assertTrue(numberOfWins >= 45);
+  }
+
+  // Tests you can win against a Random agent as Red
+  @Test (timeout=1000)
+  public void testRedWinningRandomAgent() {
+    Agent redAgent = new MyAgent(game, true);
+    Agent yellowAgent = new RandomAgent(game, false);
+    int numberOfWins = 0;
+    for (int i = 0; i < 50; i++) {
+      game.clearBoard(); 
+      while(!game.boardFull() || game.gameWon() == 'N') {
+        redAgent.move();
+        if (game.gameWon() != 'R') {
+          yellowAgent.move();
+        }
+      }
+
+      if (game.gameWon() == 'R') {
+        numberOfWins++;
+      }
+    }
+    System.out.println("You won: " + numberOfWins + " games as Red against Random");
+    // Test that you win over 90% of your games
+    assertTrue(numberOfWins >= 45);
+  }
+
+  //Tests you can win against a Random agent as Red
+  @Test (timeout=1000)
+  public void testYellowWinningRandomAgent() {
+    Agent redAgent = new RandomAgent(game, true);
+    Agent yellowAgent = new MyAgent(game, false);
+    int numberOfWins = 0;
+    for (int i = 0; i < 50; i++) {
+      game.clearBoard(); 
+      while(!game.boardFull() || game.gameWon() == 'N') {
+        redAgent.move();
+        if (game.gameWon() != 'R') {
+          yellowAgent.move();
+        }
+      }
+
+      if (game.gameWon() == 'Y') {
+        numberOfWins++;
+      }
+    }
+    System.out.println("You won: " + numberOfWins + " games as Yellow against Random");
+    // Test that you win over 90% of your games
+    assertTrue(numberOfWins >= 45);
+  }
+
+  // BONUS TODO: Write testCases to play against IntermediateAgent
+  
+  // SUPER BONUS TODO: Write testCases to playAgainst AdvancedAgent
+  
+  // SUPER BONUS TODO: Write testCases to playAgainst BrilliantAgent
+
+}
